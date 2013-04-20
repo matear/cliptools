fe52a962f40d2454645fdc95ecfa852bfe5f16a7
mostofreddy <mostofreddy@gmail.com>
2013-04-20 17:33:26 -0300

    Se crea clase Cli, clase base para aplicaciones por consola

---
+++ src/cliptools/Cli.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Cli
+ *
+ * PHP version 5.3
+ *
+ * Copyright (c) 2013 mostofreddy <mostofreddy@gmail.com>
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ *
+ * @category  Cliptools
+ * @package   Cliptools
+ * @author    Federico Lozada Mosto <mostofreddy@gmail.com>
+ * @copyright 2013 Federico Lozada Mosto <mostofreddy@gmail.com>
+ * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @link      http://www.mostofreddy.com.ar
+ */
+namespace cliptools;
+/**
+ * Clase base para crear aplicaciones que funcionen por consola
+ *
+ * @category  Cliptools
+ * @package   Cliptools
+ * @author    Federico Lozada Mosto <mostofreddy@gmail.com>
+ * @copyright 2013 Federico Lozada Mosto <mostofreddy@gmail.com>
+ * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @link      http://www.mostofreddy.com.ar
+ */
+class Cli
+{
+
+}
